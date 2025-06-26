7885e5429d25a36acbe80e9cc71c5f12a123b89c
Bharath Kumar <164044328+Namalabharath@users.noreply.github.com>
2025-06-26 20:22:27 +0530

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "https://book-store-app-backend-vj4x.onrender.com/"
+    return "https://book-store-app-backend-vj4x.onrender.com"
 }
 
 export default getBaseUrl;
