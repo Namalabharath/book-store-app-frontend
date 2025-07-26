db17cdf84a210ce5d910d5ef7d68102c5a4059ca
Bharath Kumar <164044328+Namalabharath@users.noreply.github.com>
2025-07-26 18:35:20 +0530

    Implemented Google Authentication

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -71,14 +71,14 @@ const Register = () => {
         <p className='align-baseline font-medium mt-4 text-sm'>Have an account? Please <Link to="/login" className='text-blue-500 hover:text-blue-700'>Login</Link></p>
 
         {/* google sign in */}
-{/*         <div className='mt-4'>
+        <div className='mt-4'>
             <button 
             onClick={handleGoogleSignIn}
             className='w-full flex flex-wrap gap-1 items-center justify-center bg-blue-100 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded focus:outline-none'>
             <FaGoogle  className='mr-2'/>
             Sign in with Google
             </button>
-        </div> */}
+        </div>
 
         <p className='mt-5 text-center text-gray-500 text-xs'>©2025 Book Store. All rights reserved.</p>
     </div>
