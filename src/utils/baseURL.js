6471b42cd8cdb2707840f0f6321665fd9ca90fe8
Bharath Kumar <164044328+Namalabharath@users.noreply.github.com>
2025-08-30 00:36:04 +0530

    update url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,9 +1,9 @@
 const getBaseUrl = () => {
     // Use local backend for development
-    return "http://localhost:5000/api"
+    //return "http://localhost:5000/api"
     
     // Use this for production
-    // return "https://book-store-app-backend-vj4x.onrender.com"
+     return "https://book-store-app-backend-vj4x.onrender.com"
 }
 
 export default getBaseUrl;
